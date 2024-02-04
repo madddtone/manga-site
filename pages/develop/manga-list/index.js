825095247dd837dbecdfd85b5e26cae933957f62
gonteng <madddtone@gmail.com>
2024-02-04 19:21:10 +0700

    fix layouting

--- pages/develop/manga-list/index.js
+++ pages/develop/manga-list/index.js
@@ -24,27 +24,34 @@ export default function MangaList() {
     ];
 
     return (
-      <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-2 gap-6">
-        {/* Card */}
-  
-        {manga.map(({ title, image }, index) => {
-          return (
-            <div key={index} className="rounded-xl relative">
-              
-              <Card className=''>
-                <Image
-                  className="max-h-[160px] w-20 object-cover"
-                  src={image}
-                  width={500}
-                  height={1000}
-                  alt="Naruto Doang"
-                />                
-                
-                <p className="text-gray-900">{title} </p>               
-              </Card>
-            </div> 
-          );
-        })}
+      <div className='min-w-max my-5 bg-white rounded-sm'>
+        <div className='mx-2 mt-2'>
+          <p className='text-black bold'>Latest Update</p>
+        </div>
+        <hr className='min-w-max bg-gray-800 border-1'></hr>
+        <div className="grid grid-cols-2">
+          {/* Card */}
+          {manga.map(({ title, image }, index) => {
+            return (
+                <>
+                  <div className=''>
+                    <div className='flex gap-4 mx-2 my-2'>
+                      <Image
+                        className="min-h-[160px] w-20 object-cover"
+                        src={image}
+                        width={500}
+                        height={1000}
+                        alt="Naruto Doang"
+                      />                
+                      
+                      <p className="text-gray-900 mx-2 my-2">{title} </p>               
+                    </div>
+                    <hr className='my-4 bg-gray-800 min-w-max border-1'></hr>
+                  </div>
+                </>
+            );
+          })}
+        </div>
       </div>
     );
   }
--- pages/develop/popular-list/index.js
+++ pages/develop/popular-list/index.js
@@ -20,60 +20,56 @@ export default function Favorite() {
 
 
   return (
-    <div className="flex justify-center items-center">
-    <Card className='flex max-w-max bg-gray-900'
-      >
-      
-      <h1 className='mx-7 text-gray-100'>Favorite</h1>
-      <hr className='bg-purple-100'/>
-      <div className='flex gap-4 mx-3 justify-center my-10 flex-wrap'>
-       
-       <br/>
-        <Card >
-              <Image className='border-radius-90'
-                src="/image/Shinomiya.jpg"
-                width={100}
-                height={100}
-                alt="Picture of the Author"
-              /> 
-              <p className='text-gray-300' >Judul 1</p>
-        </Card>
-
-        <Card >
-        <Image 
-                src="/image/Tuna.png"
-                width={100}
-                height={100}
-                alt="Picture of the Author"
-              /> 
-            <p className='text-gray-300' >Judul 2</p>
-        </Card>
-
-        <Card>
-        <Image 
-                src="/image/Shinomiya.jpg"
-                width={100}
-                height={100}
-                alt="Picture of the Author"
-              /> 
-            <p className='text-gray-300' >Judul 3</p>
-        </Card>
-
-        <Card className='mr-4'>
-        <Image 
-                src="/image/Shinomiya.jpg"
-                width={100}
-                height={100}
-                alt="Picture of the Author"
-              /> 
-        <p className='text-gray-300' >Judul 4</p>
-        </Card>
-
+    <div className="">
+      <div className='bg-white rounded-t-sm'>
+        <h1 className='mx-7 text-black'>Popular Today</h1>
       </div>
-      </Card>
+        <hr className='bg-black border-1'></hr>
+      <div className='flex min-w-max bg-white rounded-b-sm'>
+        <div className='flex gap-4  flex-wrap mt-2'>
+          <br/>
+          <div >
+                <Image className='border-radius-90'
+                  src="/image/Shinomiya.jpg"
+                  width={100}
+                  height={100}
+                  alt="Picture of the Author"
+                /> 
+                <p className='text-black' >Judul 1</p>
+          </div>
+
+          <div >
+          <Image 
+                  src="/image/Tuna.png"
+                  width={100}
+                  height={100}
+                  alt="Picture of the Author"
+                /> 
+              <p className='text-black' >Judul 2</p>
+          </div>
+
+          <div>
+          <Image 
+                  src="/image/Shinomiya.jpg"
+                  width={100}
+                  height={100}
+                  alt="Picture of the Author"
+                /> 
+              <p className='text-black' >Judul 3</p>
+          </div>
+
+          <div className='mr-4'>
+          <Image 
+                  src="/image/Shinomiya.jpg"
+                  width={100}
+                  height={100}
+                  alt="Picture of the Author"
+                /> 
+          <p className='text-black' >Judul 4</p>
+          </div>
+        </div>
       </div>
-   
-    
+    </div>
   )
 }
 
